bb2748a493703445ff2eb59b5add8f50aecf48d3
Egor <zuhijan@yandex.ru>
2020-10-31 18:10:38 +0600

    add types, and fix .

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', 'tsx', '.json'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     },
     mode: NODE_ENV ? NODE_ENV : "development",
     entry: path.resolve(__dirname, "src/index.tsx"),
