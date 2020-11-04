3873a900626e4f5c9e7e97616cbf468f535e689c
Egor <zuhijan@yandex.ru>
2020-11-04 14:55:28 +0600

    change ts declaration

---
+++ declaration.d.ts
@@ -0,0 +1,6 @@
+declare module '*.scss';
+declare module '*.svg';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.png';
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HTMLWebpackPlugins = require('html-webpack-plugin');
 
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
 
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
-  mode: NODE_ENV ? NODE_ENV : 'development',
+  mode: NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -28,7 +28,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           'style-loader',
-          'css-modules-typescript-loader?modules',
+          // 'css-modules-typescript-loader?modules',
           {
             loader: 'css-loader',
             options: {
