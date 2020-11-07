6cf38183c4cd44524e46adf8751a38d1333b134a
Egor <zuhijan@yandex.ru>
2020-11-07 18:09:16 +0600

    init Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react';
+import { ReactComponent as PokemonLogoSvg } from 'assets/images/logo.svg';
+import s from './Header.module.scss';
+
+interface IHeader {}
+interface IMenu {
+  id: number;
+  value: string;
+  link: string;
+}
+
+const MENU: IMenu[] = [
+  {
+    id: 1,
+    value: 'HOME',
+    link: '#',
+  },
+  {
+    id: 2,
+    value: 'Pokédex',
+    link: '#',
+  },
+  {
+    id: 3,
+    value: 'Legendaries',
+    link: '#',
+  },
+  {
+    id: 4,
+    value: 'Documentation',
+    link: '#',
+  },
+];
+const Header: FC<IHeader> = () => {
+  return (
+    <header className={s.root}>
+      <div className={s.wrap}>
+        <div className={s.pokemonLogo}>
+          <PokemonLogoSvg />
+        </div>
+      </div>
+      <nav className={s.menuWrap}>
+        {MENU.map(({ id, value, link }) => (
+          <a key={id} href={link} className={s.menuLink}>
+            {value}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
