1daaed3f1171864ef15ff6297048f18c8894ea87
Egor <zuhijan@yandex.ru>
2020-11-08 19:43:44 +0600

    complete button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import cn from 'classnames';
 
 import s from './Button.module.scss';
 
 interface IButton {
   onClick(event: React.MouseEvent<HTMLButtonElement>): void;
+  backgroundColor?: string;
+  width?: string | number;
+  size?: 'small';
 }
 
-const Button: React.FC<IButton> = ({ children, onClick }) => {
+const Button: React.FC<IButton> = ({ children, onClick, backgroundColor, width, size }) => {
   return (
-    <button className={s.root} type="button" onClick={onClick}>
+    <button
+      style={{ backgroundColor, width }}
+      className={cn(s.root, size ? s.small : '')}
+      type="button"
+      onClick={onClick}>
       {children}
     </button>
   );
