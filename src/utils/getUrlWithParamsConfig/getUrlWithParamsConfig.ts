31cfda0462f0322849ded948b5b4a95616796511
Egor <zuhijan@yandex.ru>
2020-11-27 17:58:05 +0600

    add jest, config, tests

---
+++ src/utils/getUrlWithParamsConfig/getUrlWithParamsConfig.ts
@@ -0,0 +1,24 @@
+import config from '../../config';
+
+function getUrlWithParamsConfig(endpointConfig: string, query: any) {
+  const url = {
+    ...config.client.server,
+    ...config.client.endpoint[endpointConfig as keyof typeof config.client.endpoint].uri,
+    query: {},
+  };
+
+  const pathname = Object.keys(query).reduce((acc, val) => {
+    if (acc.indexOf(`{${val}}`) !== -1) {
+      const result = acc.replace(`{${val}}`, query[val]);
+      // eslint-disable-next-line no-param-reassign
+      delete query[val];
+      return result;
+    }
+    return acc;
+  }, url.pathname);
+  url.pathname = pathname;
+  url.query = { ...query };
+  return url;
+}
+
+export default getUrlWithParamsConfig;
